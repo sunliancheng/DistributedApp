51b0620cd1a28229865fc8201bd153ddae17ecd0
wangsimin <taoyiran815579058@outlook.com>
2020-08-14 14:40:41 +0800

    08-13

---
+++ src/main/java/com/example/demo/dubboDemo/serviceImpl/Provider.java
@@ -0,0 +1,13 @@
+package com.example.demo.dubboDemo.serviceImpl;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Provider {
+    public static void main(String[] args) throws Exception {
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"DubboConf/provider.xml"});
+        context.start();
+        System.out.println("Provider started.");
+        System.in.read(); // press any key to exit
+    }
+}
